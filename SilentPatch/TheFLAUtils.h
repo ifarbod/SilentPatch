928800795ce2972d3df71480a70939806cd4d2e7
Silent <zdanio95@gmail.com>
2017-06-20 20:35:35 +0200

    Branchless GetExtendedID

--- SilentPatch/TheFLAUtils.h
+++ SilentPatch/TheFLAUtils.h
@@ -7,17 +7,19 @@ class FLAUtils
 public:
 	static int32_t GetExtendedID(const void* ptr)
 	{
-		if ( GetExtendedIDFunc != nullptr )
-			return GetExtendedIDFunc(ptr);
-		
-		uint16_t uID = *static_cast<const uint16_t*>(ptr);
-		return uID > MAX_UINT16_ID ? *static_cast<const int16_t*>(ptr) : uID;
+		return GetExtendedIDFunc(ptr);
 	}
 
 	static void Init();
 
 private:
 	static const int32_t MAX_UINT16_ID = 0xFFFD;
+
+	static int32_t GetExtendedID_Stock(const void* ptr)
+	{
+		uint16_t uID = *static_cast<const uint16_t*>(ptr);
+		return uID > MAX_UINT16_ID ? *static_cast<const int16_t*>(ptr) : uID;
+	}
 
 	static int32_t (*GetExtendedIDFunc)(const void* ptr);
 };
