07d994cc3e2bdbea5000136c275c13d679401d4c
Silent <zdanio95@gmail.com>
2017-06-20 20:35:35 +0200

    Branchless GetExtendedID

--- SilentPatch/TheFLAUtils.cpp
+++ SilentPatch/TheFLAUtils.cpp
@@ -3,7 +3,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-int32_t (*FLAUtils::GetExtendedIDFunc)(const void* ptr) = nullptr;
+int32_t (*FLAUtils::GetExtendedIDFunc)(const void* ptr) = FLAUtils::GetExtendedID_Stock;
 
 void FLAUtils::Init()
 {
